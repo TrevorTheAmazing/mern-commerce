57160dab57ad45cd5c23469a0d64d9183799c0bf
trevor clements <trevortheamazing@gmail.com>
2021-04-18 12:11:13 -0700

    user login frontend

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,78 @@
+import axios from 'axios'
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from '../constants/userConstants'
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST    
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.get('/api/users/login', { email, password }, config)
+
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        })
+    }
+}
+
+// export const addToCart = (id, qty) => async (dispatch, getState) => {
+//     const { data } = await axios.get(`/api/products/${id}`)
+
+//     dispatch({
+//         type: USER_LOGIN_FAIL,
+//         payload: {
+//             // product: data._id,
+//             // name: data.name, 
+//             // image: data.image,
+//             // price: data.price,
+//             // countInStock: data.countInStock,
+//             // qty,
+//         },
+//     })
+
+//     //localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+// }
+
+// export const addToCart = (id, qty) => async (dispatch, getState) => {
+//     const { data } = await axios.get(`/api/products/${id}`)
+
+//     dispatch({
+//         type: USER_LOGOUT,
+//         payload: {
+//             // product: data._id,
+//             // name: data.name, 
+//             // image: data.image,
+//             // price: data.price,
+//             // countInStock: data.countInStock,
+//             // qty,
+//         },
+//     })
+
+//     //localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+// }
+
+
+// export const removeFromCart = (id) => (dispatch, getState) => {
+//     dispatch({
+//         type: CART_REMOVE_ITEM,
+//         payload: id
+//     })
+
+//     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+// }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js'
 import { cartReducer } from './reducers/cartReducers.js'
+import { userLoginReducer } from './reducers/userReducers.js'
 
 //connect all the reducers, middleware, etc
 
@@ -10,12 +11,16 @@ const reducer = combineReducers({
     productList: productListReducer, 
     productDetails: productDetailsReducer, 
     cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 const cartItemsFromStorage = window.localStorage.getItem('cartItems') ? JSON.parse(window.localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = window.localStorage.getItem('userInfo') ? JSON.parse(window.localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage, },
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
 }
 
 const middleware = [thunk]
