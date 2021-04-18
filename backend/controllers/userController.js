b4a6872942fcf0dcbff239de9e1280176a571acb
trevor clements <trevortheamazing@gmail.com>
2021-04-18 08:05:09 -0700

    create new user back end

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,40 @@ const authenticateUser = asyncHandler(async(req, res) => {
     }
 })
 
+//  @desc   Register a new user
+//  @route  POST /api/users
+//  @access public
+const registerNewUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email, 
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+    
+})
+
 //  @desc   Get user profile
 //  @route  GET /api/users/profile
 //  @access private
@@ -41,21 +75,8 @@ const getUserProfile = asyncHandler(async(req, res) => {
         res.status(404)
         throw new Error('User not found')
     }
-
-    // if (user && (await user.matchPassword(password))) {
-    //     res.json({
-    //         _id: user._id,
-    //         name: user.name,
-    //         email: user.email,
-    //         isAdmin: user.isAdmin,
-    //         token: generateToken(user._id),
-    //     })
-    // } else {
-    //     res.status(401)
-    //     throw new Error('Invalid email or password')
-    // }
 })
 
 export {
-    authenticateUser, getUserProfile
+    authenticateUser, getUserProfile, registerNewUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { authenticateUser, getUserProfile } from '../controllers/userController.js'
+import { authenticateUser, registerNewUser, getUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 
+router.post('/', registerNewUser)
 router.post('/login', authenticateUser)
 router.route('/profile').get(protect, getUserProfile)
 
