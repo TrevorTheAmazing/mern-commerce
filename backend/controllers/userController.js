eae889cd89b0d13b7b2564115436220950b8de15
trevor clements <trevortheamazing@gmail.com>
2021-04-17 22:40:50 -0700

    added auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,38 @@ const authenticateUser = asyncHandler(async(req, res) => {
     }
 })
 
+//  @desc   Get user profile
+//  @route  GET /api/users/profile
+//  @access private
+const getUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById( req.user._id )
+    //res.send('Success')
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+    // if (user && (await user.matchPassword(password))) {
+    //     res.json({
+    //         _id: user._id,
+    //         name: user.name,
+    //         email: user.email,
+    //         isAdmin: user.isAdmin,
+    //         token: generateToken(user._id),
+    //     })
+    // } else {
+    //     res.status(401)
+    //     throw new Error('Invalid email or password')
+    // }
+})
+
 export {
-    authenticateUser
+    authenticateUser, getUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { authenticateUser } from '../controllers/userController.js'
+import { authenticateUser, getUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
 
 router.post('/login', authenticateUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
