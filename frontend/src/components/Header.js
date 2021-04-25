b665d8b43ac4e55acc3063919ee942230a705014
trevor clements <trevortheamazing@gmail.com>
2021-04-25 09:40:50 -0700

    admin functionality, list users

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,10 +8,8 @@ import { logout } from '../actions/userActions'
 const Header = () => {
 
     const dispatch = useDispatch()
-
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
-
     const logoutHandler = () => {
         dispatch(logout())
     }
@@ -27,21 +25,40 @@ const Header = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         <LinkContainer to="/cart">                            
-                            <Nav.Link><i className="fas fa-shopping-cart">Cart</i></Nav.Link>
+                            <Nav.Link>
+                                <i className="fas fa-shopping-cart">Cart</i>
+                            </Nav.Link>
                         </LinkContainer>
                         {userInfo ? (
                             <NavDropdown title={userInfo.name} id='username'>
                                 <LinkContainer to='/profile'>
                                     <NavDropdown.Item>Profile</NavDropdown.Item>
                                 </LinkContainer>
-                                <Dropdown.Item onClick={logoutHandler}>
+                                <NavDropdown.Item onClick={logoutHandler}>
                                     Logout
-                                </Dropdown.Item>
+                                </NavDropdown.Item>
                             </NavDropdown>
-                        ) : 
+                        ) : (
                         <LinkContainer to='/login'>
-                            <Nav.Link><i className="fas fa-user">Sign In</i></Nav.Link>
-                        </LinkContainer>}
+                            <Nav.Link>
+                                <i className="fas fa-user">Sign In</i>
+                            </Nav.Link>
+                        </LinkContainer>)}
+
+                        {userInfo && userInfo.isAdmin && (
+                            <NavDropdown title='Admin' id='adminmenu'>
+                                <LinkContainer to='/admin/userlist'>
+                                    <NavDropdown.Item>Users</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to='/admin/productlist'>
+                                    <NavDropdown.Item>Products</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to='/admin/orderlist'>
+                                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                                </LinkContainer>
+                            </NavDropdown>
+                        )}
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
