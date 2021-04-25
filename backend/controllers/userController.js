88ed548dec1df758d302ad5d2cd355e5c7762e89
trevor clements <trevortheamazing@gmail.com>
2021-04-25 15:58:40 -0700

    user management

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,29 +77,60 @@ const getUserProfile = asyncHandler(async(req, res) => {
     }
 })
 
+//  @desc   Update user
+//  @route  GET /api/users/:id
+//  @access private/admin
+const updateUser = asyncHandler(async(req, res) => {
+    const user = await User.findById( req.params.id )
+    //res.send('Success')
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (user.isAdmin !== req.body.isAdmin) {
+            user.isAdmin = req.body.isAdmin
+        } else {
+            user.isAdmin = user.isAdmin
+        }
+        
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+    
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 //  @desc   Update user profile
 //  @route  PUT /api/users/profile
 //  @access private
 const updateUserProfile = asyncHandler(async(req, res) => {
     const user = await User.findById( req.user._id )
 
-    if (user) {
-        
+    if (user) {    
         user.name = req.body.name || user.name
         user.email = req.body.email || user.email
         if (req.body.password) {
-            user.password = req.body.password
+        user.password = req.body.password
         }
 
         const updatedUser = await user.save()
-        
+
         res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: generateToken(updatedUser._id),
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser._id),
         })
+
     } else {
         res.status(404)
         throw new Error('User not found')
@@ -121,6 +152,35 @@ const getUsers = asyncHandler(async(req, res) => {
     }
 })
 
+//  @desc   Get user by ID
+//  @route  GET /api/users/:id
+//  @access private/admin
+const getUserById = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+//  @desc   Delete a user
+//  @route  DELETE /api/users/:id
+//  @access private/admin
+const deleteUser = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.params.id)
+    //res.send('Success')
+    if (user) {
+        await user.remove()
+        res.json({ message: 'User removed.' })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 export {
-    authenticateUser, getUserProfile, registerNewUser, updateUserProfile, getUsers
+    authenticateUser, getUserProfile, registerNewUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { authenticateUser, registerNewUser, getUserProfile, updateUserProfile, getUsers } from '../controllers/userController.js'
+import { authenticateUser, registerNewUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerNewUser).get(protect, admin, getUsers)
 router.post('/login', authenticateUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 export default router
