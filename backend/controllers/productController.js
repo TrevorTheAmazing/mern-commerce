2ba07f5c46dab01338131ed2dd3eadcd844a1179
trevor clements <trevortheamazing@gmail.com>
2021-05-01 22:22:35 -0700

    pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,8 @@ import Product from '../models/productModel.js'
 //  @route  GET /api/products
 //  @access public
 const getProducts = asyncHandler(async(req, res) => {
+    const pageSize = 10
+    const page = Number(req.query.pageNumber) || 1
     const keyword = req.query.keyword ? {
         name: {
             $regex: req.query.keyword,
@@ -12,10 +14,12 @@ const getProducts = asyncHandler(async(req, res) => {
         }
     } : {}
     
-    //const products = await Product.find({})
-    const products = await Product.find({ ...keyword })
+    const count = await Product.countDocuments({...keyword})
 
-    res.json(products)
+    //const products = await Product.find({})
+    const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page-1))
+
+    res.json({products, page, pages: Math.ceil(count / pageSize)})
 })
 
 //  @desc   Fetch one product
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -6,12 +6,15 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
+import Paginate from '../components/Paginate'
 
 const ProductListScreen = ({history, match}) => {
+    const pageNumber = match.params.pageNumber || 1
+
     const dispatch = useDispatch()
 
     const productList = useSelector((state) => state.productList)
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     const productDelete = useSelector((state) => state.productDelete)
     const { loading:loadingDelete, error:errorDelete, success:successDelete } = productDelete
@@ -31,10 +34,10 @@ const ProductListScreen = ({history, match}) => {
         if (successCreate) {
             history.push(`/admin/product/${createdProduct._id}/edit`)
         } else {
-            dispatch(listProducts())
+            dispatch(listProducts('', pageNumber))
         }
         
-    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct, pageNumber])
     
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure?')) {
@@ -62,6 +65,7 @@ const ProductListScreen = ({history, match}) => {
         {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
             {loading ? ( <Loader /> ) : error ? ( <Message variant='danger'>{error}</Message> ) 
              :(
+                 <>
                  <Table striped bordered hover responsive className='table-sm'>
                      <thead>
                          <tr>
@@ -95,6 +99,8 @@ const ProductListScreen = ({history, match}) => {
                         ))}
                      </tbody>
                  </Table>
+                 <Paginate pages={pages} page={page} isAdmin={true}/>
+                 </>
              )}
         </>
     )
